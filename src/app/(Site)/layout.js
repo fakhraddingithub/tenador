00d6976b04f137efaa0a9fcda3c728024b5def41
mosalehkamali <164325371+mosalehkamali@users.noreply.github.com>
2026-05-26 10:25:32 +0330

    resize favicon

--- src/app/(Site)/layout.js
+++ src/app/(Site)/layout.js
@@ -95,9 +95,9 @@ export const metadata = {
   },
 
   icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
+    icon: "/favicon.svg",
+    shortcut: "/favicon.svg",
+    apple: "/favicon.svg",
   },
 
   manifest: "/site.webmanifest",
