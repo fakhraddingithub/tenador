90a1aad7725e7c0fab5def5f3754d1ff06c7918a
mosalehkamali <mosalehkamali@gmail.com>
2026-07-09 00:03:16 +0330

    update products page in admin

--- src/app/(Admin-Panel)/p-admin/admin-products/page.jsx
+++ src/app/(Admin-Panel)/p-admin/admin-products/page.jsx
@@ -130,7 +130,7 @@ export default function AdminProducts() {
           )}
         </div>
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-5">
           {filtered.map((product, index) => (
             <motion.div
               key={product._id}
