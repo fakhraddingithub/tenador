b822748856308fe165b582c55cfa0772ec1429c9
mosalehkamali <mosalehkamali@gmail.com>
2026-09-13 09:53:39 +0200

    Fix horizontal overflow on used product detail pages

--- src/components/templates/secondHand/Usedproductclientsection.jsx
+++ src/components/templates/secondHand/Usedproductclientsection.jsx
@@ -11,10 +11,10 @@ const UsedProductClientSection = ({ product }) => {
   }, [product?.images]);
 
   return (
-    <div className="grid gap-6 sm:gap-8 lg:grid-cols-[1fr_1.5fr] lg:gap-12 xl:gap-16">
+    <div className="grid min-w-0 grid-cols-1 gap-6 sm:gap-8 lg:grid-cols-[minmax(0,1fr)_minmax(0,1.5fr)] lg:gap-12 xl:gap-16">
       {/* Top on Mobile / Left on Desktop - Gallery */}
-      <div className="w-full">
-        <div className="mx-auto max-w-md lg:max-w-none">
+      <div className="min-w-0 w-full">
+        <div className="mx-auto min-w-0 w-full max-w-md lg:max-w-none">
           <ProductGallery
             images={galleryImages}
             logo={product?.baseProduct?.brand?.icon || null}
@@ -25,7 +25,7 @@ const UsedProductClientSection = ({ product }) => {
       </div>
 
       {/* Bottom on Mobile / Right on Desktop - Info */}
-      <div className="w-full">
+      <div className="min-w-0 w-full">
         <UsedProductInfo product={product} />
       </div>
     </div>
